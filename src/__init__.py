7990da37942c2415d0c3c9f25b15360109a14427
cpizarrov <cristian.pizarroav@gmail.com>
2025-07-12 23:06:04 -0400

    Se modifica __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,6 @@
-from .data.dataset import load_taxi_data
+from .data.dataset import load_taxi_data, load_taxi_data_full
 from .features.build_features import preprocess
 from .modeling.train import train_model
-from .modeling.predict import predict, load_model, evaluate
+from .modeling.predict import predict, load_model, evaluate, evaluate_months
 from .config import features, target_col
+from .visualization.plots import plot_casos, plot_f1score
