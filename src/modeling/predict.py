7d5c18a25a8b88cb7afc4a1bf92118c5d6dabf40
cpizarrov <cristian.pizarroav@gmail.com>
2025-07-12 23:23:39 -0400

    Se actualiza función evaluate_months

--- src/modeling/predict.py
+++ src/modeling/predict.py
@@ -16,9 +16,7 @@ def evaluate(pred_probs, y_true):
     pred_labels = [p[1] for p in pred_probs.round()]
     return f1_score(y_true, pred_labels)
 
-def evaluate_months(model, year="2020"):
-    df_full = load_taxi_data_full(year)
-    df_full = preprocess(df_full, target_col)
+def evaluate_months(model, df_full, year="2020"):
     df_full["tpep_pickup_datetime"] = pd.to_datetime(df_full["tpep_pickup_datetime"])
 
     results = []
