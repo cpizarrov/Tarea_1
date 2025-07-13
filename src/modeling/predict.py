370f282e8968f164aa010bb67f1c1f5b0753ef8a
cpizarrov <cristian.pizarroav@gmail.com>
2025-07-13 13:30:12 -0400

    Se edita función evaluate_months() en predict.py para hacer flexible la selección de meses. Se amplía la descarga de datos para todo 2020. en dataset.py

--- src/modeling/predict.py
+++ src/modeling/predict.py
@@ -16,19 +16,21 @@ def evaluate(pred_probs, y_true):
     pred_labels = [p[1] for p in pred_probs.round()]
     return f1_score(y_true, pred_labels)
 
-def evaluate_months(model, df_full, year="2020"):
-    df_full["tpep_dropoff_datetime"] = pd.to_datetime(df_full["tpep_dropoff_datetime"])
+def evaluate_months(model, df, year="2020", start_month=1, end_month=12):
+    df = df.copy()
+    df["tpep_dropoff_datetime"] = pd.to_datetime(df["tpep_dropoff_datetime"])
 
     results = []
 
-    for month in range(1, 13):
-        df_month = df_full[df_full["tpep_dropoff_datetime"].dt.month == month]
+    for month in range(start_month, end_month + 1):
+        df_month = df[df["tpep_dropoff_datetime"].dt.month == month]
 
         if df_month.empty:
             continue
 
         X = df_month[features]
         y_true = df_month[target_col]
+
         pred_probs = predict(model, X)
         score = evaluate(pred_probs, y_true)
 
